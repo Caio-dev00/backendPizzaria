1c261f66454a7fb385aabd71c3dd9722d7dab503
Caio Marques <caiohenrique1146@gmail.com>
2024-10-11 09:52:55 -0300

    att

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,7 @@ import fileUpload from 'express-fileupload';
 const app = express();
 app.use(express.json());
 
-const allowedOrigins = [process.env.FRONTEND_URL]
-
-app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true
-}));
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log('Origin:', req.headers.origin); // Verifique a origem aqui
